35f1169b47b114028253e1d5a09012278f020e23
jonathan <jonathan41@hotmail.com>
2016-10-19 20:11:06 +0100

    added the list form

--- WindowsFormsApplication1/WindowsFormsApplication1/frmDVDList.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmDVDList.Designer.cs
@@ -31,6 +31,16 @@
             this.mnuStrip = new System.Windows.Forms.MenuStrip();
             this.mnuBack = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lstDVDlist = new System.Windows.Forms.ListBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cboRetalRate = new System.Windows.Forms.ComboBox();
+            this.cboGenre = new System.Windows.Forms.ComboBox();
+            this.cboAgeRating = new System.Windows.Forms.ComboBox();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.lblAgeRating = new System.Windows.Forms.Label();
             this.mnuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,7 +51,7 @@
             this.mnuExit});
             this.mnuStrip.Location = new System.Drawing.Point(0, 0);
             this.mnuStrip.Name = "mnuStrip";
-            this.mnuStrip.Size = new System.Drawing.Size(396, 29);
+            this.mnuStrip.Size = new System.Drawing.Size(411, 29);
             this.mnuStrip.TabIndex = 2;
             this.mnuStrip.Text = "mnuStrip";
             // 
@@ -59,14 +69,111 @@
             this.mnuExit.Text = "Exit";
             this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
             // 
+            // lstDVDlist
+            // 
+            this.lstDVDlist.FormattingEnabled = true;
+            this.lstDVDlist.Location = new System.Drawing.Point(12, 192);
+            this.lstDVDlist.Name = "lstDVDlist";
+            this.lstDVDlist.Size = new System.Drawing.Size(387, 381);
+            this.lstDVDlist.TabIndex = 3;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(28, 45);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(41, 13);
+            this.lblSearch.TabIndex = 4;
+            this.lblSearch.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(75, 42);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(163, 20);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(75, 149);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cboRetalRate
+            // 
+            this.cboRetalRate.FormattingEnabled = true;
+            this.cboRetalRate.Location = new System.Drawing.Point(75, 122);
+            this.cboRetalRate.Name = "cboRetalRate";
+            this.cboRetalRate.Size = new System.Drawing.Size(149, 21);
+            this.cboRetalRate.TabIndex = 22;
+            // 
+            // cboGenre
+            // 
+            this.cboGenre.FormattingEnabled = true;
+            this.cboGenre.Location = new System.Drawing.Point(75, 95);
+            this.cboGenre.Name = "cboGenre";
+            this.cboGenre.Size = new System.Drawing.Size(149, 21);
+            this.cboGenre.TabIndex = 20;
+            // 
+            // cboAgeRating
+            // 
+            this.cboAgeRating.FormattingEnabled = true;
+            this.cboAgeRating.Location = new System.Drawing.Point(75, 68);
+            this.cboAgeRating.Name = "cboAgeRating";
+            this.cboAgeRating.Size = new System.Drawing.Size(149, 21);
+            this.cboAgeRating.TabIndex = 18;
+            // 
+            // lblRate
+            // 
+            this.lblRate.AutoSize = true;
+            this.lblRate.Location = new System.Drawing.Point(8, 125);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(64, 13);
+            this.lblRate.TabIndex = 21;
+            this.lblRate.Text = "Rental Rate";
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Location = new System.Drawing.Point(8, 98);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(36, 13);
+            this.lblGenre.TabIndex = 19;
+            this.lblGenre.Text = "Genre";
+            // 
+            // lblAgeRating
+            // 
+            this.lblAgeRating.AutoSize = true;
+            this.lblAgeRating.Location = new System.Drawing.Point(9, 71);
+            this.lblAgeRating.Name = "lblAgeRating";
+            this.lblAgeRating.Size = new System.Drawing.Size(60, 13);
+            this.lblAgeRating.TabIndex = 17;
+            this.lblAgeRating.Text = "Age Rating";
+            // 
             // frmDVDList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 464);
+            this.ClientSize = new System.Drawing.Size(411, 585);
+            this.Controls.Add(this.cboRetalRate);
+            this.Controls.Add(this.cboGenre);
+            this.Controls.Add(this.cboAgeRating);
+            this.Controls.Add(this.lblRate);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.lblAgeRating);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.lstDVDlist);
             this.Controls.Add(this.mnuStrip);
             this.Name = "frmDVDList";
             this.Text = "frmDVDList";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmDVDList_FormClosed);
+            this.Load += new System.EventHandler(this.frmDVDList_Load);
             this.mnuStrip.ResumeLayout(false);
             this.mnuStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -79,5 +186,15 @@
         private System.Windows.Forms.MenuStrip mnuStrip;
         private System.Windows.Forms.ToolStripMenuItem mnuBack;
         private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ListBox lstDVDlist;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cboRetalRate;
+        private System.Windows.Forms.ComboBox cboGenre;
+        private System.Windows.Forms.ComboBox cboAgeRating;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.Label lblAgeRating;
     }
 }
--- WindowsFormsApplication1/WindowsFormsApplication1/frmDVDList.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/frmDVDList.cs
@@ -34,5 +34,59 @@ namespace WindowsFormsApplication1
         {
             Application.Exit();
         }
+
+        private void frmDVDList_Load(object sender, EventArgs e)
+        {
+            lstDVDlist.Visible = false;
+
+            cboAgeRating.Items.Add("General");
+            cboAgeRating.Items.Add("Parental Guidance");
+            cboAgeRating.Items.Add("12");
+            cboAgeRating.Items.Add("15");
+            cboAgeRating.Items.Add("18");
+
+            cboGenre.Items.Add("Action");
+            cboGenre.Items.Add("Adventure");
+            cboGenre.Items.Add("Animation");
+            cboGenre.Items.Add("Biography");
+            cboGenre.Items.Add("Comedy");
+            cboGenre.Items.Add("Crime");
+            cboGenre.Items.Add("Children");
+            cboGenre.Items.Add("Documentary");
+            cboGenre.Items.Add("Drama");
+            cboGenre.Items.Add("Family");
+            cboGenre.Items.Add("Fantasy");
+            cboGenre.Items.Add("History");
+            cboGenre.Items.Add("Horror");
+            cboGenre.Items.Add("Mystery");
+            cboGenre.Items.Add("Romance");
+            cboGenre.Items.Add("Sci - Fi");
+            cboGenre.Items.Add("Thriller");
+            cboGenre.Items.Add("War");
+            cboGenre.Items.Add("Western");
+
+            cboRetalRate.Items.Add("New Release");
+            cboRetalRate.Items.Add("Old Release");
+            cboRetalRate.Items.Add("New Childrens Release");
+            cboRetalRate.Items.Add("Old Childrens Release");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            List<string> _items = new List<string>();
+            lstDVDlist.Visible = true;
+            if (txtSearch.Text == "King")
+            {
+                _items.Add("The Lord of the Rings: The Return of the King");
+                _items.Add("The Lion King");
+                _items.Add("The King's Speech");
+                lstDVDlist.DataSource = _items;
+            }
+        }
+
+        private void frmDVDList_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            parent.Show();
+        }
     }
 }
